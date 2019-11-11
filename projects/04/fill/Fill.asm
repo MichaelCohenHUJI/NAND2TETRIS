7c20885261e23604d2c2e145ae8bff43d821fdfb
Michael Cohen <mikico101@gmail.com>
2019-11-11 22:28:10 +0200

    finished fill and mult

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,41 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
+//
 
-// Put your code here.
+  (INNERLOOP)
+  @KBD
+  D=M //D is the input
+  @switch
+  M=-1 //switch on
+  @TURNSWITCH //jump if keyboard bit is >0
+  D;JNE
+  @switch //if keyboard bit is 0 switch off
+  M=0
+
+  (TURNSWITCH)
+  @SCREEN
+  D=A
+  @screenbus //save the address of first screen bit
+  M=D
+
+  (ITERLOOP)
+  @switch
+  D=M
+  @screenbus
+  A=M
+  M=D   //switch to requested state
+  @screenbus
+  M=M+1 //save the value of next screen bit
+  D=M
+  @24576
+  D=A-D //check if all screen bits are switched
+  @ITERLOOP //if not, continue to next bit
+  D;JNE
+
+  @INNERLOOP //start the process again
+  0;JMP
